ab766a24e25ef612a45a4e2859098e364a1c72c7
James-Kwao <jameskwaw1@gmail.com>
2024-04-05 21:12:21 +0000

    Superadmin interface updated

--- Ticket Reservation/src/Main/Display/SuperAdminInterface.java
+++ Ticket Reservation/src/Main/Display/SuperAdminInterface.java
@@ -3,6 +3,7 @@ package Main.Display;
 import Main.Database.DB;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.geom.RoundRectangle2D;
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,10 +14,16 @@ import java.util.Objects;
 public class SuperAdminInterface {
     private final DB db;
     private final ArrayList<RoundRectangle2D.Float> userBorders;
+    private int mouseX, mouseY;
+    private char display;
+    private RoundRectangle2D.Float loginBtn;
+    private Color logoutClr;
 
     public SuperAdminInterface(DB db) {
         this.db = db;
+        display = 'T';
         userBorders = new ArrayList<>();
+        logoutClr = new Color(0x284138);
     }
 
     private void drawTotalUsers(int xPos, int yPos, Graphics2D g2) {
@@ -25,6 +32,7 @@ public class SuperAdminInterface {
             if (files != null) {
                 String delimiter = "                  ";
                 for (int i = 0; i < files.length; i++) {
+
                     if (!files[i].getName().equals(db.getCurrentUser().getParentFile().getName())) {
                         BufferedReader reader = new BufferedReader(new FileReader(
                                 Objects.requireNonNull(files[i].listFiles())[1]));
@@ -32,13 +40,13 @@ public class SuperAdminInterface {
                         String w = users[0] + delimiter + users[1] + delimiter + users[2] + delimiter + users[4];
                         reader.close();
 
-
                         userBorders.add(new RoundRectangle2D.Float(xPos, yPos, g2.getFontMetrics().stringWidth(w) + 30,
                                 50, 20, 20));
                         g2.setColor(new Color(0x284138));
                         g2.fill(userBorders.get(i));
                         g2.setStroke(new BasicStroke(3));
-                        g2.setColor(new Color(0xFF9900));
+                        g2.setColor(userBorders.get(i).contains(mouseX, mouseY) ? new Color(0xFF9900) :
+                                new Color(0x35464B));
                         g2.draw(userBorders.get(i));
 
                         g2.setColor(Color.WHITE);
@@ -53,8 +61,14 @@ public class SuperAdminInterface {
         }
     }
 
-    public void drawInterface(Graphics2D g2) {
+    public void drawInterface(Graphics2D g2, Point getMousePosition) {
         int x = 20, y = 70;
+        //mouse position on screen
+        try {
+            mouseX = getMousePosition == null ? -1 : getMousePosition.x;
+            mouseY = getMousePosition == null ? -1 : getMousePosition.y;
+        } catch (Exception ignored) {
+        }
 
         g2.setFont(new Font("Callibre", Font.PLAIN, 15));
 
@@ -68,6 +82,23 @@ public class SuperAdminInterface {
 
         drawTotalUsers((int) (x + border.getArcWidth() / 2), y + 40, g2);
 
+        //create the sign-out button
+        x = x + MainFrame.SCREEN_SIZE.width - 240;
+        y = 18;
+        int h = 30, w = 60;
+        loginBtn = new RoundRectangle2D.Float(x, y, w + 10, h, 10, 10);
+        g2.setStroke(new BasicStroke(3));
+        g2.setColor(logoutClr);
+        g2.fill(loginBtn);
+        g2.setColor(loginBtn.contains(mouseX, mouseY) ? new Color(0x35464B) : new Color(0xFF9900));
+        g2.draw(loginBtn);
+        //draw text
+        g2.setFont(new Font("Bell MT", Font.PLAIN, 15));
+        g2.setColor(Color.WHITE);
+        x += (w + 10) / 2 - g2.getFontMetrics().stringWidth("Sign out") / 2;
+        y += h / 2 + g2.getFontMetrics().getAscent() / 2 - 1;
+        g2.drawString("Sign out", x, y);
+
         g2.setStroke(new BasicStroke(4));
         if (!userBorders.isEmpty()) {
             border = new RoundRectangle2D.Float(710, userBorders.getFirst().getBounds().y,
@@ -79,4 +110,25 @@ public class SuperAdminInterface {
             g2.draw(border);
         }
     }
+
+    public void mouseClicked(MouseEvent e) {
+        display = 'T';
+        if (loginBtn != null && loginBtn.contains(e.getPoint()))
+            display = 'L';
+    }
+
+    public void mousePressed(MouseEvent e) {
+        if (loginBtn != null && loginBtn.contains(e.getPoint()))
+            logoutClr = logoutClr.darker().darker();
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        if (loginBtn != null && loginBtn.contains(e.getPoint()))
+            logoutClr = new Color(0x284138);
+    }
+
+    public char getDisplay() {
+        return display;
+    }
+
 }
